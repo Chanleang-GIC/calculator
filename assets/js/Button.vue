c7f4e9ecc7ae0048fd733a1b840e38c080a06670
thavorac <thavorac.chun@gmail.com>
2018-02-20 15:31:47 +0700

    Calculator v1

---
+++ assets/js/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button>Hello</button>
+</template>
+<script>
+    export default {
+        name: 'MyButton',
+        data() {
+            return {
+                checked: false,
+                title: 'Check me'
+            }
+        },
+        methods: {
+            check() {
+                this.checked = !this.checked;
+            }
+        }
+    }
+</script>
